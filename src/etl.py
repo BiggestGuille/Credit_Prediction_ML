6ee0cc841ad922f57ec1e0bb8f89455bdfaf028f
Jemartri44 <jemartri@gmail.com>
2024-09-25 18:45:29 +0200

    First real commit

---
+++ src/etl.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib.pyplot as plt
+import statsmodels as sm
+
+df = pd.read_csv('../data/EstudioCrediticio_TrainP.csv')
+print(df.head(10))
